93945d5c1f5f349d3f1f064bf2e155b7aa9b5a2e
PrasannaPrasanth <115775638+PrasannaPrasanth@users.noreply.github.com>
2023-04-04 04:00:02 -0600

    Add files via upload

---
+++ vending_5star/fivestar_vending/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vending_5star;
+
+namespace fivestar_vending
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            vending_machine v = new vending_machine();
+            Console.WriteLine(v);
+            v.fivestar();
+
+            Console.WriteLine();
+
+            v.setrupee(5);
+            v.fivestar();
+
+            v.setrupee(20);
+            v.fivestar();
+
+            v.setrupee(10);
+            v.fivestar();
+
+
+        }
+    }
+}
---
+++ vending_5star/vending_5star/vending_machine.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vending_5star
+{
+    public class vending_machine
+    {
+        money _rupee;
+
+        public money Rupee
+        {
+            get { return _rupee; }
+            set { _rupee = value; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Rupees: {0}",this._rupee);
+        }
+        public vending_machine()
+        {
+            Rupee = money.zero;
+        }
+
+        
+        public void fivestar()
+        {
+            if (Rupee == money.five)
+            {
+                Console.WriteLine("Take a 5 rupee 5-STAR");
+            }
+            else if (Rupee == money.ten)
+            {
+                Console.WriteLine("Take a 10 rupee 5-STAR");
+            }
+            else if (Rupee == money.twenty)
+            {
+                Console.WriteLine("Take a 20 rupee 5-STAR");
+            }
+            else if (Rupee == money.zero)
+            {
+                Console.WriteLine("please give a amount");
+            }
+        }
+
+        public void setrupee(int s)
+        {
+            if (s == 5)
+            {
+                Rupee = money.five;
+            }
+            if (s == 10)
+            {
+                Rupee = money.ten;
+            }
+            if (s == 20)
+            {
+                Rupee = money.twenty;
+            }
+        }
+        
+
+
+    }
+}
